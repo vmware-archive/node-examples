8e4d8efbefe75eff289bf474e59ad4cd7d431e38
Matthew Reddington <mreddington@pivotal.io>
2019-12-16 11:39:38 -0800

    WIP. Code complete but untestested.

---
+++ put-get-remove/scripts/shutdownGemFire.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Attempt to set APP_HOME
+# Resolve links: $0 may be a link
+PRG="$0"
+# Need this for relative symlinks.
+while [ -h "$PRG" ] ; do
+    ls=`ls -ld "$PRG"`
+    link=`expr "$ls" : '.*-> \(.*\)$'`
+    if expr "$link" : '/.*' > /dev/null; then
+        PRG="$link"
+    else
+        PRG=`dirname "$PRG"`"/$link"
+    fi
+done
+SAVED="`pwd`"
+cd "`dirname \"$PRG\"`/.." >&-
+APP_HOME="`pwd -P`"
+cd "$SAVED" >&-
+
+pushd ${APP_HOME}/data/locator4 > /dev/null
+
+gfsh -e "connect --locator=locator[10337]" -e "shutdown --include-locators=true --time-out=15"
+
+popd > /dev/null
