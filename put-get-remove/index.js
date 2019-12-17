f7e4e072167db0ab5d2b6c869741191c0599b5d2
Vince Ford <vford@pivotal.io>
2019-12-16 16:01:03 -0800

    WIP. Addressed minor bugs, configured logging to reduce noisy output

--- put-get-remove/index.js
+++ put-get-remove/index.js
@@ -1,31 +1,43 @@
 var gemfire = require('gemfire');
 
 async function put_get_remove_example() {
-    console.log('Creating a cache factory')
-    const cacheFactory = gemfire.createCacheFactory()
-    console.log('Creating a cache')
-    const cache = await cacheFactory.create()
-    console.log('Creating a pool factory')
-    const poolFactory = await cache.getPoolManager().createFactory()
 
+    //configure and create client cache
+    console.log('Creating a cache factory')
+    cacheFactory = gemfire.createCacheFactory()
+    cacheFactory.set("log-file","data/nodeClient.log")
+    cacheFactory.set("log-level","config")
+    cacheFactory.set("statistic-archive-file","data/clientStatArchive.gfs")
+    console.log('Creating a cache')
+    cache = await cacheFactory.create()
+    console.log('Creating a pool factory')
+    poolFactory = await cache.getPoolManager().createFactory()
     console.log('Configuring the pool factory to find a locator at localhost:10337')
     poolFactory.addLocator('localhost', 10337)
     console.log('Creating a pool')
-    poolFactory.create("pool")
+    poolFactory.create('pool')
 
+    //create region for data
     console.log('Creating a region called \'test\' of type \'PROXY\' connected to the pool named \'pool\'')
-    const region = cache.createRegion("test", { type: 'PROXY', poolName: 'pool' })
+    region = await cache.createRegion("test", { type: 'PROXY', poolName: 'pool' })
 
+    //put, get and remove data from region
     console.log('Putting key \'foo\' into region \'test\' with the value \'bar\'')
     await region.put('foo', 'bar')
     console.log('Getting value from region \'test\' with key \'foo\'. The expected value is going to be: \'bar\'')
     var result = await region.get('foo')
-    console.log('The value retrieved is: \'' + value + '\'')
+    console.log('The value retrieved is: \'' + result + '\'')
     console.log('Removing key \'foo\' from region \'test\'')
     await region.remove('foo')
-    console.log('Getting value from region \'test\' with key \'foo\'. The expected value is going to be: undefined')
+    console.log('Getting value from region \'test\' with key \'foo\'. The expected value is going to be: null')
     result = await region.get('foo')
-    console.log('The value retrieved is: \'' + value + '\'')
+    console.log('The value retrieved is: \'' + result + '\'')
+
+    //done with cache then close it
+    cache.close()
+
+    //exit nodejs
+    console.log('To exit: CTRL-C')
 }
 
 put_get_remove_example()
