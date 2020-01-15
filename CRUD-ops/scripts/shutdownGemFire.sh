469a98f9e71183543babbb2830e2a1400cd903f9
Vince Ford <vford@pivotal.io>
2020-01-15 10:14:32 -0800

    Clean up start-up scripts

--- CRUD-ops/scripts/shutdownGemFire.sh
+++ CRUD-ops/scripts/shutdownGemFire.sh
@@ -1,22 +1,6 @@
 #!/bin/bash
 
-# Attempt to set APP_HOME
-# Resolve links: $0 may be a link
-PRG="$0"
-# Need this for relative symlinks.
-while [ -h "$PRG" ] ; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
-    if expr "$link" : '/.*' > /dev/null; then
-        PRG="$link"
-    else
-        PRG=`dirname "$PRG"`"/$link"
-    fi
-done
-SAVED="`pwd`"
-cd "`dirname \"$PRG\"`/.." >&-
 APP_HOME="`pwd -P`"
-cd "$SAVED" >&-
 
 pushd ${APP_HOME}/data/locator > /dev/null
 
