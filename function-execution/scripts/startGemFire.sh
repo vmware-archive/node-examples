fc07115cd8bd8f14d25de2ea83a7a2b3c8ffada9
Vince Ford <vford@pivotal.io>
2020-01-15 16:52:54 -0800

    Make function-execution example work

--- function-execution/scripts/startGemFire.sh
+++ function-execution/scripts/startGemFire.sh
@@ -1,88 +1,68 @@
 #!/bin/bash
 
-# Attempt to set APP_HOME
-# Resolve links: $0 may be a link
-PRG="$0"
-# Need this for relative symlinks.
-while [ -h "$PRG" ] ; do
-    ls=`ls -ld "$PRG"`
-    link=`expr "$ls" : '.*-> \(.*\)$'`
-    if expr "$link" : '/.*' > /dev/null; then
-        PRG="$link"
-    else
-        PRG=`dirname "$PRG"`"/$link"
-    fi
-done
-SAVED="`pwd`"
-cd "`dirname \"$PRG\"`/.." >&-
 APP_HOME="`pwd -P`"
-cd "$SAVED" >&-
-
-function waitForPort {
-
-    (exec 6<>/dev/tcp/${HOSTNAME}/$1) &>/dev/null
-    while [[ $? -ne 0 ]]
-    do
-        echo -n "."
-        sleep 1
-        (exec 6<>/dev/tcp/${HOSTNAME}/$1) &>/dev/null
-    done
-}
 
 function launchLocator() {
-
+    echo ""
+    echo "*** Start Locator ***"
     mkdir -p ${APP_HOME}/data/locator
-    pushd ${APP_HOME}/data/locator
+    pushd ${APP_HOME}/data/locator > /dev/null
 
-    gfsh -e "start locator --name=locator --port=10337 --dir=${APP_HOME}/data/locator" &
+    gfsh -e "start locator --name=locator --port=10337 --dir=${APP_HOME}/data/locator"
 
-    popd
+    popd > /dev/null
 }
 
 function launchServer() {
-
+    echo ""
+    echo "*** Launch Server ***"
     mkdir -p ${APP_HOME}/data/server
-    pushd ${APP_HOME}/data/server
+    pushd ${APP_HOME}/data/server > /dev/null
 
-    gfsh -e "connect --locator=localhost[10337]" -e "start server --locators=localhost[10337] --server-port=40404 --name=server --dir=${APP_HOME}/data/server" &
+    gfsh -e "connect --locator=localhost[10337]" -e "start server --locators=localhost[10337] --server-port=40404 --name=server --dir=${APP_HOME}/data/server"
 
-    popd
+    popd > /dev/null
 }
 
 function buildFunction() {
+    echo ""
+    echo "*** Build Function Jar ***"
+    export CLASSPATH=${GEODE_HOME}/lib/*
 
-    export CLASSPATH=${GEODE_HOME}/lib/geode-core-9.9.0.jar:${GEODE_HOME}/lib/geode-common-9.9.0.jar
-    pushd ${APP_HOME}/src
-    echo ${GEODE_HOME}
-    java -version
-    javac com/vmware/example/SumRegion.java
-    jar -cvf SumRegion.jar *
-    popd
+    pushd ${APP_HOME}/src > /dev/null
+      echo "GEODE_HOME=${GEODE_HOME}"
+      echo "CLASSPATH=${CLASSPATH}"
+      java -version
+      javac com/vmware/example/SumRegion.java
+      jar -cvf SumRegion.jar *
+    popd > /dev/null
 }
 
 function deployFunction() {
+    echo ""
+    echo "*** Deploy Function Jar ***"
+    gfsh -e "connect --locator=localhost[10337]" -e "deploy --jar=${APP_HOME}/src/SumRegion.jar"
+}
 
-    gfsh -e "connect --locator=localhost[10337]" -e "deploy --jar=${APP_HOME}/src/SumRegion.jar" &
+function createRegion(){
+  echo ""
+  echo "*** Create Partition Region \"test\" ***"
+  pushd ${APP_HOME}/data/server > /dev/null
+
+  gfsh -e "connect --locator=localhost[10337]" -e "create region --name=test --type=PARTITION"
+
+  popd > /dev/null
 }
 
 buildFunction
+sleep 10
 
 launchLocator
-
-waitForPort 10337
-
 sleep 10
 
 launchServer
+sleep 10
 
-wait
+createRegion
 
 deployFunction
-
-wait
-
-pushd ${APP_HOME}/data/server
-
-gfsh -e "connect --locator=localhost[10337]" -e "create region --name=test --type=PARTITION"
-
-popd
