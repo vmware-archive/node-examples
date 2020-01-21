330254b5aca285e2a9d651bca3d32d24f673482e
Vince Ford <vford@pivotal.io>
2020-01-21 15:34:26 -0800

    Fix scripts making for cleaner start-up of servers

--- authenticate/scripts/startGemFire.sh
+++ authenticate/scripts/startGemFire.sh
@@ -20,9 +20,9 @@ function launchLocator() {
     mkdir -p ${APP_HOME}/data/locator
     pushd ${APP_HOME}/data/locator > /dev/null
 
-      gfsh -e "start locator --connect=false --name=locator --port=10337 --dir=${APP_HOME}/data/locator --classpath=${APP_HOME}/src/security.jar --J=-Dgemfire.security-manager=securitymanager.SimpleSecurityManager" &
+      gfsh -e "start locator --connect=false --name=locator --port=10337 --dir=${APP_HOME}/data/locator --classpath=${APP_HOME}/src/security.jar --J=-Dgemfire.security-manager=securitymanager.SimpleSecurityManager"
       gfsh -e "connect --locator=localhost[10337]" -e "configure pdx --read-serialized=true"
-      
+
     popd > /dev/null
 }
 
@@ -32,7 +32,7 @@ function launchServer() {
     mkdir -p ${APP_HOME}/data/server
     pushd ${APP_HOME}/data/server > /dev/null
 
-      gfsh -e "connect --locator=localhost[10337] --user=root --password=root-password" -e "start server --user=root --password=root-password --locators=localhost[10337] --server-port=40404 --name=server --dir=${APP_HOME}/data/server  --classpath=${APP_HOME}/src/security.jar --J=-Dgemfire.security-manager=securitymanager.SimpleSecurityManager" &
+      gfsh -e "connect --locator=localhost[10337] --user=root --password=root-password" -e "start server --user=root --password=root-password --locators=localhost[10337] --server-port=40404 --name=server --dir=${APP_HOME}/data/server  --classpath=${APP_HOME}/src/security.jar --J=-Dgemfire.security-manager=securitymanager.SimpleSecurityManager"
 
     popd > /dev/null
 }
@@ -63,6 +63,6 @@ sleep 10
 
 launchServer
 
-wait
+sleep 10
 
 createRegion
