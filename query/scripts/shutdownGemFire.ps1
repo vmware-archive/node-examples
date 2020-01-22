f129f1e4e17b7eab5c094a343868f2bc9f7484f0
Mike Martell <mmartell@pivotal.io>
2020-01-22 14:02:23 -0800

    Add powershell scripts for query.

---
+++ query/scripts/shutdownGemFire.ps1
@@ -0,0 +1,27 @@
+$APP_HOME = pwd
+
+$GFSH_PATH = ""
+if (Get-Command gfsh -ErrorAction SilentlyContinue)
+{
+    $GFSH_PATH = "gfsh"
+}
+else
+{
+    if (-not (Test-Path env:GEODE_HOME))
+    {
+        Write-Host "Could not find gfsh.  Please set the GEODE_HOME path. e.g. "
+        Write-Host "(Powershell) `$env:GEODE_HOME = <path to Geode>"
+        Write-Host " OR"
+        Write-Host "(Command-line) set %GEODE_HOME% = <path to Geode>"
+    }
+    else
+    {
+        $GFSH_PATH = "$env:GEODE_HOME\bin\gfsh.bat"
+    }
+}
+
+pushd $APP_HOME/data/locator
+
+Invoke-Expression "$GFSH_PATH -e 'connect --locator=localhost[10337]' -e 'shutdown --include-locators=true --time-out=15'"
+
+popd
