7e66230665a99ee2e3ab27814039335501880dd6
Blake Bender <bbender@pivotal.io>
2020-02-10 15:23:04 -0800

    [#171190428]: Add Apache license to source and scripts

--- authenticate/scripts/startGemFire.ps1
+++ authenticate/scripts/startGemFire.ps1
@@ -1,3 +1,17 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 $APP_HOME = pwd
 
 function build_security_jar() {
@@ -5,7 +19,7 @@ function build_security_jar() {
     echo "*** Build SimpleSecurityManager ***"
     $env:CLASSPATH="$env:GEODE_HOME\lib\*"
     echo "CLASSPATH=$env:CLASSPATH"
-    pushd $APP_HOME/src 
+    pushd $APP_HOME/src
       javac securitymanager/SimpleSecurityManager.java
       jar -vcf security.jar *
     popd
